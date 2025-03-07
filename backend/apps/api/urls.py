5afa70938b70189e74ec47a2cabe1ccc6f6e8bb1
nramirez <nramirez@livvolab.com>
2025-03-07 11:34:00 +0000

    Corrección docker-compose-local-dev.yml

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import MicrosoftLogin, TokenObtainPairView, TokenRefreshView
+from .views import MicrosoftLogin
 
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView, name='token_refresh'),
-    path("microsoft/", MicrosoftLogin, name="microsoft_login"),
+    # path('token/', TokenObtainPairView(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView, name='token_refresh'),
+    path("microsoft/", MicrosoftLogin.as_view(), name="microsoft_login"),
 ]
